13e6a9282d48579c92a0b8f209c299310545ebba
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2025-06-15 19:10:28 +0300

    Finished Tile class methods

--- source/tile.cpp
+++ source/tile.cpp
@@ -2,7 +2,7 @@
 
 Tile::Tile(int x, int y) {
     coord = { x, y };
-	coord_prev = coord;
+    coord_prev = coord;
 }
 
 Tile::~Tile() {}
@@ -28,8 +28,8 @@ void Tile::SetPrev(int x, int y) {
 }
 
 void Tile::SetTile(int new_fig, int new_clr) {
-	fig = new_fig;
-	clr = new_clr;
+    fig = new_fig;
+    clr = new_clr;
 }
 
 void Tile::SetSpecial(bool new_spcl) {
@@ -49,11 +49,11 @@ POINT Tile::GetPrev() {
 }
 
 int Tile::GetColor() {
-	return clr;
+    return clr;
 }
 
 int Tile::GetFigure() {
-	return fig;
+    return fig;
 }
 
 bool Tile::GetSpecial() {
@@ -71,8 +71,286 @@ void Tile::MovePiece(Tile* new_tile) {
     SetTile(NONE, NONE);
 }
 
-void Tile::SpreadThreat(Tile board[][BOARDSIZE]) {}
+void Tile::SpreadThreat(Tile board[][BOARDSIZE]) {
+    switch (fig) {
+    case KING:
+        for (int y = max(coord.y - 1, 0); y < min(coord.y + 2, BOARDSIZE - 1); y++) {
+            for (int x = max(coord.x - 1, 0); x < min(coord.x + 2, BOARDSIZE - 1); x++) {
+                board[x][y].SetThreat(true, clr + 1);
+            }
+        }
+        break;
+    case QUEEN:
+        ThreatHor(board);
+        ThreatVert(board);
+        ThreatDiag(board);
+        break;
+    case ROOK:
+        ThreatHor(board);
+        ThreatVert(board);
+        break;
+    case BISHOP:
+        ThreatDiag(board);
+        break;
+    case KNIGHT:
+        if (coord.x + 1 < BOARDSIZE && coord.y + 2 < BOARDSIZE)
+            board[coord.x + 1][coord.y + 2].SetThreat(true, clr + 1);
+        if (coord.x + 2 < BOARDSIZE && coord.y + 1 < BOARDSIZE)
+            board[coord.x + 2][coord.y + 1].SetThreat(true, clr + 1);
+        if (coord.x - 1 >= 0 && coord.y + 2 < BOARDSIZE)
+            board[coord.x - 1][coord.y + 2].SetThreat(true, clr + 1);
+        if (coord.x + 2 < BOARDSIZE && coord.y - 1 >= 0)
+            board[coord.x + 2][coord.y - 1].SetThreat(true, clr + 1);
+        if (coord.x - 2 >= 0 && coord.y + 1 < BOARDSIZE)
+            board[coord.x - 2][coord.y + 1].SetThreat(true, clr + 1);
+        if (coord.x + 1 < BOARDSIZE && coord.y - 2 >= 0)
+            board[coord.x + 1][coord.y - 2].SetThreat(true, clr + 1);
+        if (coord.x - 2 >= 0 && coord.y - 1 >= 0)
+            board[coord.x - 2][coord.y - 1].SetThreat(true, clr + 1);
+        if (coord.x - 1 >= 0 && coord.y - 2 >= 0)
+            board[coord.x - 1][coord.y - 2].SetThreat(true, clr + 1);
+        break;
+    case PAWN:
+        if (coord.y - clr) {
+            if (coord.x - 1 >= 0)
+                board[coord.x - 1][coord.y - clr].SetThreat(true, clr + 1);
+            if (coord.x + 1 < BOARDSIZE)
+                board[coord.x + 1][coord.y - clr].SetThreat(true, clr + 1);
+        }
+        break;
+    }
+}
+
+void Tile::ThreatHor(Tile board[][BOARDSIZE]) {
+    for (int x = coord.x - 1; x >= 0; x--) {
+        board[x][coord.y].SetThreat(true, clr + 1);
+        if (board[x][coord.y].GetColor()) {
+            break;
+        }
+    }
+    for (int x = coord.x + 1; x < BOARDSIZE; x++) {
+        board[x][coord.y].SetThreat(true, clr + 1);
+        if (board[x][coord.y].GetColor()) {
+            break;
+        }
+    }
+}
+
+void Tile::ThreatVert(Tile board[][BOARDSIZE]) {
+    for (int y = coord.y - 1; y >= 0; y--) {
+        board[coord.x][y].SetThreat(true, clr + 1);
+        if (board[coord.x][y].GetColor()) {
+            break;
+        }
+    }
+    for (int y = coord.y + 1; y < BOARDSIZE; y++) {
+        board[coord.x][y].SetThreat(true, clr + 1);
+        if (board[coord.x][y].GetColor()) {
+            break;
+        }
+    }
+}
+
+void Tile::ThreatDiag(Tile board[][BOARDSIZE]) {
+    for (int y = coord.y - 1, x = coord.x - 1; y >= 0 && x >= 0; y--, x--) {
+        board[x][y].SetThreat(true, clr + 1);
+        if (board[x][y].GetColor()) {
+            break;
+        }
+    }
+    for (int y = coord.y + 1, x = coord.x - 1; y < BOARDSIZE && x >= 0; y++, x--) {
+        board[x][y].SetThreat(true, clr + 1);
+        if (board[x][y].GetColor()) {
+            break;
+        }
+    }
+    for (int y = coord.y - 1, x = coord.x + 1; y >= 0 && x < BOARDSIZE; y--, x++) {
+        board[x][y].SetThreat(true, clr + 1);
+        if (board[x][y].GetColor()) {
+            break;
+        }
+    }
+    for (int y = coord.y + 1, x = coord.x + 1; y < BOARDSIZE && x < BOARDSIZE; y++, x++) {
+        board[x][y].SetThreat(true, clr + 1);
+        if (board[x][y].GetColor()) {
+            break;
+        }
+    }
+}
 
 bool Tile::IsMoveAllowed(Tile board[][BOARDSIZE], Tile* dest) {
-	return false;
+    if (dest->GetColor() == clr) {
+        return false;
+    }
+    switch (fig) {
+    case KING:
+        if (coord.x - dest->GetCoord().x <= 1 &&
+            coord.x - dest->GetCoord().x >= -1 &&
+            coord.y - dest->GetCoord().y <= 1 &&
+            coord.y - dest->GetCoord().y >= -1) {
+            return true;
+        }
+        else if (spcl && !GetThreat(1 - clr)) { //castling
+            if (dest->GetCoord().x == 2 &&
+                board[0][coord.y].GetSpecial()) {
+                for (int x = 1; x < coord.x; x++) {
+                    if (board[x][coord.y].GetColor() ||
+                        board[x][coord.y].GetThreat(1 - clr)) {
+                        return false;
+                    }
+                }
+                board[0][coord.y].MovePiece(&board[3][coord.y]);
+                return true;
+            }
+            else if (dest->GetCoord().x == BOARDSIZE - 2 &&
+                board[BOARDSIZE - 1][coord.y].GetSpecial()) {
+                for (int x = coord.x + 1; x < BOARDSIZE - 1; x++) {
+                    if (board[x][coord.y].GetColor() ||
+                        board[x][coord.y].GetThreat(1 - clr)) {
+                        return false;
+                    }
+                }
+                board[BOARDSIZE - 1][coord.y].MovePiece(&board[BOARDSIZE - 3][coord.y]);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+        break;
+    case QUEEN:
+        if (IsVertClear(board, dest) ||
+            IsHorClear(board, dest) ||
+            IsDiagClear(board, dest)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        break;
+    case ROOK:
+        if (IsVertClear(board, dest) ||
+            IsHorClear(board, dest)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        break;
+    case BISHOP:
+        if (IsDiagClear(board, dest)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        break;
+    case KNIGHT:
+        if ((coord.x - dest->GetCoord().x) * (coord.x - dest->GetCoord().x) == 4 &&
+            (coord.y - dest->GetCoord().y) * (coord.y - dest->GetCoord().y) == 1 ||
+            (coord.x - dest->GetCoord().x) * (coord.x - dest->GetCoord().x) == 1 &&
+            (coord.y - dest->GetCoord().y) * (coord.y - dest->GetCoord().y) == 4) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        break;
+    case PAWN:
+        if (coord.x == dest->GetCoord().x && //move forward 1 space
+            coord.y == dest->GetCoord().y + clr &&
+            dest->GetFigure() == NONE) {
+            return true;
+        }
+        else if (coord.x == dest->GetCoord().x && //move forward 2 spaces
+            coord.y == dest->GetCoord().y + (clr * 2) &&
+            dest->GetFigure() == NONE &&
+            GetSpecial()) {
+            return true;
+        }
+        else if ((coord.x - dest->GetCoord().x) * (coord.x - dest->GetCoord().x) == 1 && //take enemy figure
+            coord.y == dest->GetCoord().y + clr &&
+            dest->GetColor() == -clr) {
+            return true;
+        }
+        else if ((coord.x - dest->GetCoord().x) * (coord.x - dest->GetCoord().x) == 1 && //en passaint
+            coord.y == dest->GetCoord().y + clr &&
+            dest->GetColor() == NONE) {
+            Tile* spec_tile = &board[dest->GetCoord().x][dest->GetCoord().y + clr];
+            if (spec_tile->GetFigure() == PAWN &&
+                spec_tile->GetColor() == -clr &&
+                spec_tile->GetPrev().x == spec_tile->GetCoord().x &&
+                spec_tile->GetPrev().y == spec_tile->GetCoord().y - (clr * 2)) {
+                spec_tile->SetTile(NONE, NONE);
+                return true;
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+        break;
+    }
+    return false;
+}
+
+bool Tile::IsHorClear(Tile board[][BOARDSIZE], Tile* dest) {
+    if (coord.y == dest->GetCoord().y) {//horizontal
+        for (int i = min(coord.x, dest->GetCoord().x) + 1; i < max(coord.x, dest->GetCoord().x); i++) {
+            if (board[i][coord.y].GetFigure() != NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Tile::IsVertClear(Tile board[][BOARDSIZE], Tile* dest) {
+    if (coord.x == dest->GetCoord().x) {//vertical
+        for (int i = min(coord.y, dest->GetCoord().y) + 1; i < max(coord.y, dest->GetCoord().y); i++) {
+            if (board[coord.x][i].GetFigure() != NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Tile::IsDiagClear(Tile board[][BOARDSIZE], Tile* dest) {
+    if (coord.x < dest->GetCoord().x && //diagonal 1
+        coord.y < dest->GetCoord().y &&
+        coord.x - dest->GetCoord().x == coord.y - dest->GetCoord().y ||
+        coord.x > dest->GetCoord().x && coord.y > dest->GetCoord().y &&
+        coord.x - dest->GetCoord().x == coord.y - dest->GetCoord().y) {
+        for (int x = min(coord.x, dest->GetCoord().x) + 1, y = min(coord.y, dest->GetCoord().y) + 1;
+            x < max(coord.x, dest->GetCoord().x) && y < max(coord.y, dest->GetCoord().y); x++, y++) {
+            if (board[x][y].GetFigure() != NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else if (coord.x < dest->GetCoord().x && //diagonal 2
+        coord.y > dest->GetCoord().y &&
+        coord.x - dest->GetCoord().x == dest->GetCoord().y - coord.y) {
+        for (int x = coord.x + 1, y = coord.y - 1; x < dest->GetCoord().x && y > dest->GetCoord().y; x++, y--) {
+            if (board[x][y].GetFigure() != NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else if (coord.x > dest->GetCoord().x && //diagonal 3
+        coord.y < dest->GetCoord().y &&
+        coord.x - dest->GetCoord().x == dest->GetCoord().y - coord.y) {
+        for (int x = coord.x - 1, y = coord.y + 1; x > dest->GetCoord().x && y < dest->GetCoord().y; x--, y++) {
+            if (board[x][y].GetFigure() != NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
